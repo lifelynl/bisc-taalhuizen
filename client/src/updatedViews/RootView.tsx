2bae7f07d063afadf7821fe6083d338303d83166
gorkem <gorkem.kockaya@gmail.com>
2021-03-19 21:10:32 +0300

    feat(root-view): add boilerplate

--- client/src/updatedViews/RootView.tsx
+++ client/src/updatedViews/RootView.tsx
@@ -1,1 +1,36 @@
-export {}
+import React, { useContext } from 'react'
+import { SessionContext } from '../components/Providers/SessionProvider/context'
+import { UserContext } from '../components/Providers/UserProvider/context'
+import { Type } from '../components/Providers/UserProvider/types'
+
+export const RootView: React.FunctionComponent = () => {
+    const context = useContext(SessionContext)
+    const user = useContext(UserContext).user
+
+    // if no session || no user
+    if (!context.accesstoken || !user) {
+        // TODO: return authorization view
+        return null
+    }
+
+    // if user type is bisc
+    if (user.environment === Type.bisc) {
+        // TODO
+        return null
+    }
+
+    // if user type is aanbieder
+    if (user.environment === Type.aanbieder) {
+        // TODO
+        return null
+    }
+
+    // if user type is taalhuis
+    if (user.environment === Type.taalhuis) {
+        // TODO
+        return null
+    }
+
+    // TODO: not found view (do we have one?)
+    return null
+}
--- client/src/updatedViews/routes.ts
+++ client/src/updatedViews/routes.ts
@@ -1,3 +1,28 @@
-// TODO: place all the routes here
-
-export {}
+export const routes = {
+    authorization: {
+        index: '/auth',
+        login: '/auth/login',
+        forgotPassword: '/auth/forgot-password',
+        resetPassword: '/auth/reset-password',
+        loggedOut: '/auth/logged-out',
+    },
+    bisc: {
+        taalhuis: {},
+        aanbieder: {},
+        reports: {},
+        participants: {},
+    },
+    taalhuis: {
+        reports: {},
+        participants: {},
+    },
+    aanbieder: {
+        reports: {},
+        participants: {},
+    },
+    app: {
+        // for design review/check only
+        translationsExample: '/translations-example',
+        kitchensink: '/kitchensink',
+    },
+}
